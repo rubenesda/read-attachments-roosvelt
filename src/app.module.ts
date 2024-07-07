297034c0d4ef401653bcdaa5f5cb3a5c37e8726a
Ruben Dario Escandon <rescandon@vovsolutions.com>
2024-07-07 13:43:26 -0500

    add(mail-controller): extract the attachments from a eml file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailController } from './mail/mail.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MailController],
   providers: [AppService],
 })
 export class AppModule {}
