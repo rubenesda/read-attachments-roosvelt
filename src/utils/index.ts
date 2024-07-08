92cf944acf772ce1af1101a87bf2be042e71f9ae
Ruben Dario Escandon <rubenesda@gmail.com>
2024-07-08 00:57:03 -0500

    add(documentation): add comments on mail.controller.ts and add another util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,9 @@
-export const supportedExtension = (filename) => {
+/**
+ * Small method that will determine if the filename has a valid extension
+ * @param filename - string - filename with extension
+ * @returns - boolean
+ */
+export const supportedExtension = (filename: string) => {
   const supportedExtensions = ['.json'];
 
   if (filename) {
@@ -7,4 +12,19 @@ export const supportedExtension = (filename) => {
   }
 
   return false;
+};
+
+/**
+ * Small method that will centralize different messages
+ * @param message_code - string - a small message code
+ * @returns - string - the full message
+ */
+export const parseMsgCode = (message_code: string) => {
+  const messages = {
+    NO_ATTACHMENTS: 'No attachments found in Email',
+    NO_VALID_ATTACHMENTS:
+      'No attachments with supported file extension found in Email',
+  };
+
+  return messages[message_code];
 };
